622a84f8c1dd0026115f4ef26dcda162706df483
austinjkovach <kovach.a.j@gmail.com>
2023-01-05 21:19:27 -0800

    tutorial complete

--- levels.js
+++ levels.js
@@ -18,35 +18,38 @@ TUTORIAL LEVELS
 const levels = [
   {
     grid: [
+      [0, t(2, true, true), 0],
       [0, 0, 0],
-      [0, 0, 0],
-      [0, l(), 0],
-    ],
-    targetPoints: 0,
-    description: 'Click the button to activate the laser',
-  },
-  {
-    grid: [
-      [0, l(2), 0],
-      [0, 0, 0],
-      [0, t(), 0],
+      [0, l(0, true, true), 0],
     ],
     targetPoints: 1,
-    description: 'Just click the fucking button',
+    description:
+      "Click the button to activate the laser! Shoot the laser at the target's light to activate it",
   },
   {
     grid: [
-      [0, t(2), 0],
-      [0, l(2, true), 0],
+      [0, 0, 0],
+      [l(1, true, true), 0, t(2, true, true)],
       [0, 0, 0],
     ],
     tokenBank: [],
     targetPoints: 1,
-    description: 'Look, now you have to rotate it',
+    description:
+      'If the target is not facing the correct direction, the laser will not activate it. You can click on a piece to rotate it',
   },
   {
     grid: [
-      [0, t(2), 0],
+      [0, 0, 0],
+      [l(0, true, true), 0, t(3, false, true)],
+      [0, 0, 0],
+    ],
+    tokenBank: [],
+    targetPoints: 1,
+    description: 'Some pieces cannot be rotated.',
+  },
+  {
+    grid: [
+      [0, t(2, true, true), 0],
       [0, 0, 0],
       [0, 0, 0],
     ],
@@ -56,12 +59,74 @@ const levels = [
   },
   {
     grid: [
-      [0, t(2), 0],
+      [0, t(2, true, false), 0],
       [0, 0, 0],
-      [0, 0, 0],
+      [l(0, true, true), 0, 0],
+    ],
+    tokenBank: [],
+    targetPoints: 1,
+    description: 'Some pieces cannot be moved',
+  },
+  {
+    grid: [
+      [0, t(2, false, false), 0],
+      [0, c(0), 0],
+      [l(0, true, true), 0, 0],
+    ],
+    tokenBank: [],
+    targetPoints: 1,
+    description:
+      'The laser must touch every token in order to beat the level. Checkpoints allow the laser to pass through only from a specific angle',
+  },
+  {
+    grid: [
+      [0, t(1), 0, c(1), m(0)],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
     ],
     tokenBank: [l(0, true, true)],
     targetPoints: 1,
-    description: 'the same thing again, plz',
+    description: 'Mirrors reflect the laser at a 90-degree angle',
+  },
+  {
+    grid: [
+      [0, t(1), 0, c(1), m(0)],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ],
+    tokenBank: [l(0, true, true)],
+    targetPoints: 1,
+    description:
+      'Beam Splitters split the laser in two, allowing it to pass through as well as reflect at an angle',
+  },
+  {
+    grid: [
+      [0, m(1), 0, 0, t(3)],
+      [0, 0, 0, 0, 0],
+      [0, b(1), 0, 0, t(3)],
+      [0, 0, 0, 0, 0],
+      [0, l(0), 0, 0, 0],
+    ],
+    tokenBank: [],
+    targetPoints: 2,
+    description:
+      'Beam Splitters split the laser in two, allowing it to pass through as well as reflect at an angle',
+  },
+  {
+    grid: [
+      [0, 0, 0, 0, 0],
+      [x(), 0, 0, 0, 0],
+      [c(), 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [m(0), x(), t(3), 0, 0],
+    ],
+    tokenBank: [l(0, true, true)],
+    targetPoints: 1,
+    description:
+      'Cell Blockers: lasers can pass through, but other tokens cannot. Cell Blockers do not need to be touched in order to beat a level',
   },
 ];
